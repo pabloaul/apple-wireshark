a429e6fb46319166fb2594c85e205c2f8b9f8eb7
Nojus <git@nojus.org>
2026-05-18 02:17:49 +0200

    uarp: update type names

--- plugins/uarp/uarp.lua
+++ plugins/uarp/uarp.lua
@@ -1,26 +1,9 @@
-uarp_proto = Proto("uarp", "Apple Unified Accessory Restore Protocol")
+uarp_proto = Proto("uarp", "Apple Universal Accessory Restore Protocol")
 
-local cmd_type = {
-    [0x0000] = "Hello",
-    [0x0001] = "Connect",
-    [0x0002] = "Conenct Response",
-    [0x0003] = "Read Register?",
-    [0x0004] = "Read Register Response?",
-
-    [0x0005] = "?",
-    [0x0006] = "?",
-    [0x0007] = "?",
-    [0x0009] = "?",
-    [0x000D] = "?",
-    [0x000F] = "?",
-    [0x0011] = "?",
-    [0x0012] = "?",
-
-    [0xFFFF] = "?",
-}
+local enums = require("uarp_enums")
 
 local f = rtbuddy_proto.fields
-    f.type = ProtoField.uint16("uarp.type", "Type", base.HEX, cmd_type)
+    f.type = ProtoField.uint16("uarp.type", "Type", base.HEX, enums.message_type)
     f.length = ProtoField.uint16("uarp.length", "Length", base.DEC)
     f.seq = ProtoField.uint16("uarp.seq", "Sequence", base.DEC)
     f.data = ProtoField.bytes("uarp.data", "Data", base.NONE)
@@ -29,11 +12,12 @@ function uarp_proto.dissector(buffer, pinfo, tree)
     if buffer():len() == 0 then return end
     pinfo.cols.protocol = uarp_proto.name
 
-    local subtree = tree:add(uarp_proto, buffer(), "Apple Unified Accessory Restore Protocol")
+    local subtree = tree:add(uarp_proto, buffer(), "Apple Universal Accessory Restore Protocol")
     local offset = 0
 
     local type = buffer(offset, 2):uint()
     subtree:add(f.type, buffer(offset, 2))
+    pinfo.cols.info:set("[" .. (enums.message_type[type] or "Undiscovered") .. "] ")
     offset = offset + 2
 
     local length = buffer(offset, 2):uint()
