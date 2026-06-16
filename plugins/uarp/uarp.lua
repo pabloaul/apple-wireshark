224c42776ea807da48de39f0bd26c732673794ed
Nojus <git@nojus.org>
2026-06-16 19:08:41 +0200

    uarp: annotate fields

--- plugins/uarp/uarp.lua
+++ plugins/uarp/uarp.lua
@@ -8,7 +8,15 @@ local f = rtbuddy_proto.fields
     f.seq = ProtoField.uint16("uarp.seq", "Sequence", base.DEC)
     f.data = ProtoField.bytes("uarp.data", "Data", base.NONE)
     f.version = ProtoField.uint16("uarp.version", "Version", base.HEX)
+    f.status = ProtoField.uint16("uarp.status", "Status", base.HEX, enums.status_code)
     f.fw_apply_status = ProtoField.uint16("uarp.fw_apply_status", "Firmware Application Status", base.HEX, enums.firmware_application_status)
+    f.information_property = ProtoField.uint16("uarp.information_property", "Information Property", base.HEX, enums.accessory_property)
+    f.asset_id = ProtoField.uint16("uarp.asset_id", "Asset ID", base.DEC)
+    f.asset_tag = ProtoField.uint32("uarp.asset_tag", "Asset Tag", base.HEX)
+    f.endpoint_id = ProtoField.uint16("uarp.endpoint_id", "Endpoint ID", base.DEC)
+    f.data_request_offset = ProtoField.uint32("uarp.data_request_offset", "Data Request Offset", base.HEX)
+    f.data_request_length = ProtoField.uint32("uarp.data_request_length", "Data Request Length", base.DEC)
+    f.data_actual_length = ProtoField.uint32("uarp.data_actual_length", "Data Actual Length", base.DEC)
     f.unknown = ProtoField.bytes("uarp.unknown", "Unknown", base.NONE)
 
 function uarp_proto.dissector(buffer, pinfo, tree)
@@ -30,41 +38,41 @@ function uarp_proto.dissector(buffer, pinfo, tree)
     subtree:add(f.seq, buffer(offset, 2))
     offset = offset + 2
 
-    if type == 0x00 then
-    elseif type == 0x01 then
+    if type == 0x00 then -- Sync
+    elseif type == 0x01 then -- Version Discovery Request
         subtree:add(f.version, buffer(offset, 2))
         offset = offset + 2
-    elseif type == 0x02 then
-        subtree:add(f.unknown, buffer(offset, 2))
+    elseif type == 0x02 then -- Version Discovery Response
+        subtree:add(f.status, buffer(offset, 2))
         offset = offset + 2
 
         subtree:add(f.version, buffer(offset, 2))
         offset = offset + 2
-    elseif type == 0x03 then
-        subtree:add(f.unknown, buffer(offset, 4))
+    elseif type == 0x03 then -- Information Request
+        subtree:add(f.information_property, buffer(offset, 4))
         offset = offset + 4
-    elseif type == 0x04 then
-        subtree:add(f.unknown, buffer(offset, 2))
+    elseif type == 0x04 then -- Information Response
+        subtree:add(f.status, buffer(offset, 2))
         offset = offset + 2
 
-        subtree:add(f.unknown, buffer(offset, 2))
-        offset = offset + 2
+        subtree:add(f.information_property, buffer(offset, 4))
+        offset = offset + 4
 
-        local len = buffer(offset, 2):uint()
-        offset = offset + 2
+        local len = buffer(offset, 4):uint()
+        offset = offset + 4
 
         if len ~= 0 then
             subtree:add(f.data, buffer(offset, len))
             offset = offset + len
         end
-    elseif type == 0x05 or type == 0x0D then
-        subtree:add(f.unknown, buffer(offset, 4))
+    elseif type == 0x05 or type == 0x0D then -- Asset Available Notification (Ack)
+        subtree:add(f.asset_tag, buffer(offset, 4))
         offset = offset + 4
 
         subtree:add(f.unknown, buffer(offset, 2))
         offset = offset + 2
 
-        subtree:add(f.unknown, buffer(offset, 2))
+        subtree:add(f.asset_id, buffer(offset, 2))
         offset = offset + 2
 
         subtree:add(f.unknown, buffer(offset, 4))
@@ -76,86 +84,90 @@ function uarp_proto.dissector(buffer, pinfo, tree)
         subtree:add(f.unknown, buffer(offset, 4))
         offset = offset + 4
 
-        subtree:add(f.unknown, buffer(offset, 8))
-        offset = offset + 8
-
-        subtree:add(f.unknown, buffer(offset, 2))
-        offset = offset + 2
-    elseif type == 0x06 then
-        subtree:add(f.unknown, buffer(offset, 2))
-        offset = offset + 2
+        subtree:add(f.unknown, buffer(offset, 4))
+        offset = offset + 4
 
         subtree:add(f.unknown, buffer(offset, 4))
         offset = offset + 4
 
         subtree:add(f.unknown, buffer(offset, 2))
         offset = offset + 2
-    elseif type == 0x07 then
-        subtree:add(f.unknown, buffer(offset, 2))
+    elseif type == 0x06 then -- Data Request
+        subtree:add(f.asset_id, buffer(offset, 2))
         offset = offset + 2
 
-        subtree:add(f.unknown, buffer(offset, 2))
-        offset = offset + 2
-
-        subtree:add(f.unknown, buffer(offset, 4))
+        subtree:add(f.data_request_offset, buffer(offset, 4))
         offset = offset + 4
 
-        subtree:add(f.unknown, buffer(offset, 2))
+        subtree:add(f.data_request_length, buffer(offset, 2))
+        offset = offset + 2
+    elseif type == 0x07 then -- Data Response
+        subtree:add(f.status, buffer(offset, 2))
+        offset = offset + 2
+
+        subtree:add(f.asset_id, buffer(offset, 2))
+        offset = offset + 2
+
+        subtree:add(f.data_request_offset, buffer(offset, 4))
+        offset = offset + 4
+
+        subtree:add(f.data_request_length, buffer(offset, 2))
         offset = offset + 2
 
         local len = buffer(offset, 2):uint()
+        subtree:add(f.data_actual_length, buffer(offset, 2))
         offset = offset + 2
 
         subtree:add(f.data, buffer(offset, len))
         offset = offset + len
-    elseif type == 0x09 then
-        subtree:add(f.unknown, buffer(offset, 2))
+    elseif type == 0x09 then -- Asset Processing Notification
+        subtree:add(f.asset_id, buffer(offset, 2))
         offset = offset + 2
 
         subtree:add(f.unknown, buffer(offset, 2))
         offset = offset + 2
-    elseif type == 0x0A then
-    elseif type == 0x0B then
+    elseif type == 0x0A then -- Apply Staged Assets Request
+    elseif type == 0x0B then -- Apply Staged Assets Response
         subtree:add(f.fw_apply_status, buffer(offset, 2))
         offset = offset + 2
 
         subtree:add(f.unknown, buffer(offset, 2))
         offset = offset + 2
-    elseif type == 0x0F then
-        subtree:add(f.unknown, buffer(offset, 2))
+    elseif type == 0x0F then -- Asset Processing Notification Ack
+        subtree:add(f.asset_id, buffer(offset, 2))
         offset = offset + 2
 
         subtree:add(f.unknown, buffer(offset, 2))
         offset = offset + 2
-    elseif type == 0x11 then
-        subtree:add(f.unknown, buffer(offset, 4))
+    elseif type == 0x11 then -- Dynamic Asset Solicitation
+        subtree:add(f.asset_tag, buffer(offset, 4))
         offset = offset + 4
-    elseif type == 0x12 then
+    elseif type == 0x12 then -- Dynamic Asset Solicitation Ack
         subtree:add(f.unknown, buffer(offset, 4))
         offset = offset + 4
 
-        subtree:add(f.unknown, buffer(offset, 4))
+        subtree:add(f.asset_tag, buffer(offset, 4))
         offset = offset + 4
-    elseif type == 0x17 then
-        subtree:add(f.unknown, buffer(offset, 2))
+    elseif type == 0x17 then -- Downstream Endpoint Reachable
+        subtree:add(f.endpoint_id, buffer(offset, 2))
         offset = offset + 2
-    elseif type == 0x18 then
-        subtree:add(f.unknown, buffer(offset, 2))
+    elseif type == 0x18 then -- Downstream Endpoint Reachable Ack
+        subtree:add(f.status, buffer(offset, 2))
         offset = offset + 2
 
-        subtree:add(f.unknown, buffer(offset, 2))
+        subtree:add(f.endpoint_id, buffer(offset, 2))
         offset = offset + 2
-    elseif type == 0x1B then -- downstream endpoint message
-        subtree:add(f.unknown, buffer(offset, 2))
+    elseif type == 0x1B then -- Downstream Endpoint Message
+        subtree:add(f.endpoint_id, buffer(offset, 2))
         offset = offset + 2
 
         offset = offset + Dissector.get("uarp"):call(buffer(offset):tvb(), pinfo, tree)
         pinfo.cols.info:prepend("[Downstream] ")
-    elseif type == 0x1C then
+    elseif type == 0x1C then -- Downstream Endpoint Message Ack
         subtree:add(f.unknown, buffer(offset, 2))
         offset = offset + 2
 
-        subtree:add(f.unknown, buffer(offset, 2))
+        subtree:add(f.endpoint_id, buffer(offset, 2))
         offset = offset + 2
     end
 
