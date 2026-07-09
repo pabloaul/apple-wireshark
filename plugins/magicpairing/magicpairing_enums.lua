209561d28fb6c94884df00f578326e05d378482c
Nojus <git@nojus.org>
2026-07-09 20:05:08 +0200

    magicpairing: more status enums

--- plugins/magicpairing/magicpairing_enums.lua
+++ plugins/magicpairing/magicpairing_enums.lua
@@ -9,16 +9,20 @@ e.type = {
     [0xff] = "Status",
 }
 
+-- bluetoothd -> "NO_KEYS_FOR_PEER"
 e.status = {
-    [0] = "Success",
-    [1] = "Internal Error",
-    [2] = "Key Not Found",
-    [3] = "Invalid Parameters",
-    [4] = "Pairing Busy",
-    [5] = "Unsupported Version",
-    [6] = "Operation Timed Out",
-    [7] = "Failed Verification",
-    [8] = "No Keys for Peer"
+    [0x00] = "Success",
+    [0x01] = "Internal Error",
+    [0x02] = "Key Not Found",
+    [0x03] = "Invalid Parameters",
+    [0x04] = "Pairing Busy",
+    [0x05] = "Unsupported Version",
+    [0x06] = "Operation Timed Out",
+    [0x07] = "Failed Verification",
+    [0x08] = "No Keys for Peer",
+    [0x09] = "No Resources",
+    [0x0A] = "Disconnection",
+    [0xFF] = "Invalid",
 }
 
 e.key_type = {
